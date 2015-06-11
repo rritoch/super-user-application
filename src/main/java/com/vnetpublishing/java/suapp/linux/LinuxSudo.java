336d5fbab683e78f560daf0efd8707551857a1b4
rritoch <rritoch@gmail.com>
2015-06-11 17:12:11 +0800

    Release 0.0.1

---
+++ src/main/java/com/vnetpublishing/java/suapp/linux/LinuxSudo.java
@@ -0,0 +1,7 @@
+package com.vnetpublishing.java.suapp.linux;
+
+import com.vnetpublishing.java.suapp.posix.PosixSudo;
+
+public class LinuxSudo extends PosixSudo {
+
+}
---
+++ src/main/java/com/vnetpublishing/java/suapp/linux/LinuxSuperUserDetector.java
@@ -0,0 +1,7 @@
+package com.vnetpublishing.java.suapp.linux;
+
+import com.vnetpublishing.java.suapp.posix.PosixSuperUserDetector;
+
+public class LinuxSuperUserDetector extends PosixSuperUserDetector {
+
+}
